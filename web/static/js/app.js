cd97c0988bc0aad8d8aacf5e7e8912105f6b18ad
bearski <santosh.sewlal@gmail.com>
2017-12-13 12:38:19 +0200

    set up for elm

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -21,5 +21,5 @@ import "phoenix_html"
 // import socket from "./socket"
 
 // Set up our Elm App
-const elmDiv = document.querySelector(‘#elm-container’);
+const elmDiv = document.querySelector('#elm-container');
 const elmApp = Elm.App.embed(elmDiv);
