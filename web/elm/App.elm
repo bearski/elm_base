3eb8f1374de35681860fc9a688325a2614e3ff74
bearski <santosh.sewlal@gmail.com>
2017-12-13 17:58:26 +0200

    getting started with elm

--- web/elm/App.elm
+++ web/elm/App.elm
@@ -1,6 +1,29 @@
 module App exposing (..)
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
+-- MAIN
+main : Html a
 main =
-  text "Hello from Elm!"
+  view model
+
+-- MODEL
+model : String
+model =
+  "World"
+
+-- VIEW
+view : String -> Html a
+view person =
+  div []
+    [ h2 [] [ text ("Todo List For: " ++ person) ]
+    , ul []
+      [ li [] [ text "Sample Todo Item"]
+      ]
+    , input
+      [type' "text"
+      , placeholder "New Todo Item"
+      ] []
+    , button [] [ text "Add Item" ]
+    ]
